5b2d073a2a1a481f235a4ba2891e40848cd72821
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2022-02-05 18:09:16 +0530

    Connected to wifi successfully

--- nodemcu_writer/nodemcu_writer.ino
+++ nodemcu_writer/nodemcu_writer.ino
@@ -1,9 +1,22 @@
+#include "ESP8266WiFi.h"
+
+const char* ssid = "paya1"; // Write here your router's username
+const char* password = "naina123"; // Write here your router's passward
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
 
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("Wifi connected");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //Serial.println("Hello World");
 }
