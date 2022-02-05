72843aeca684d2b677071ffdcc78147df61208b2
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2022-02-05 19:04:31 +0530

    Write numbers from 1-10 successfully

--- nodemcu_writer/nodemcu_writer.ino
+++ nodemcu_writer/nodemcu_writer.ino
@@ -1,7 +1,13 @@
 #include "ESP8266WiFi.h"
+#include "ThingSpeak.h"
 
 const char* ssid = "paya1"; // Write here your router's username
 const char* password = "naina123"; // Write here your router's passward
+
+unsigned long myChannelNumber = 1;
+const char * myWriteAPIKey = "WXYBG42UPC56AK4E";
+
+WiFiClient client;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,8 +21,16 @@ void setup() {
 
   Serial.println("Wifi connected");
   Serial.println(WiFi.localIP());
+
+  ThingSpeak.begin(client);
 }
 
 void loop() {
   //Serial.println("Hello World");
+ 
+ 
+  for(int i=0; i<=10; i++){
+    ThingSpeak.writeField(myChannelNumber, 1, i, myWriteAPIKey);
+    delay(7000);
+  }
 }
